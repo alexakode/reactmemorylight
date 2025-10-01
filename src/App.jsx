635d557fc40b8cb87bac1c85d74c96e04cd7e536
alexakode <alexanderm.kodehode@gmail.com>
2025-10-01 14:27:28 +0200

    Made basic game function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import shuffleDeck from "./utils/shuffleDeck";
 import Card from "./components/Card";
 import "./App.css";
@@ -13,11 +13,46 @@ export default function App() {
       matched: false,
     }))
   );
+  const [flippedCards, setFlippedCards] = useState([]);
   const handleCardClick = (id) => {
-    setDeck((prev) =>
-      prev.map((card) => (card.id === id ? { ...card, flipped: true } : card))
+    if (flippedCards.length === 2 || deck.find((c) => c.id === id).flipped)
+      return;
+    const updatedDeck = deck.map((card) =>
+      card.id === id ? { ...card, flipped: true } : card
     );
+    setDeck(updatedDeck);
+    setFlippedCards([...flippedCards, id]);
   };
+  useEffect(() => {
+    if (flippedCards.length === 2) {
+      const [firstId, secondId] = flippedCards;
+      const firstCard = deck.find((card) => card.id === firstId);
+      const secondCard = deck.find((card) => card.id === secondId);
+      if (firstCard.value === secondCard.value) {
+        // match
+        setDeck((prev) =>
+          prev.map((card) =>
+            card.id === firstId || card.id === secondId
+              ? { ...card, matched: true }
+              : card
+          )
+        );
+        setFlippedCards([]);
+      } else {
+        // Ikke match, snu tilbake etter delay
+        setTimeout(() => {
+          setDeck((prev) =>
+            prev.map((card) =>
+              card.id === firstId || card.id === secondId
+                ? { ...card, flipped: false }
+                : card
+            )
+          );
+          setFlippedCards([]);
+        }, 1000); //1 sekund
+      }
+    }
+  }, [flippedCards, deck]);
   return (
     <div className="game-board">
       {deck.map((card) => (
