90612d10f48e8153ef0f61b1f90ea2f523ed84d3
alexakode <alexanderm.kodehode@gmail.com>
2025-10-01 13:52:51 +0200

    fixed minimal initial style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import "./Card.css"
-// import ./Card.css for stil (ikke lagd ennå)
+import "./Card.css";
 export default function Card({ value, flipped, matched, onClick }) {
   const handleClick = () => {
     if (!flipped && !matched) {
@@ -8,14 +7,12 @@ export default function Card({ value, flipped, matched, onClick }) {
     }
   };
   return (
-    <div
-      className={`card ${flipped || matched ? "flipped" : ""}`}
-      onClick={handleClick}
-    >
-      <div className="card-inner">
+    <div className="card" onClick={handleClick}>
+      {flipped || matched ? (
         <div className="card-front">{value}</div>
+      ) : (
         <div className="card-back">❓️</div>
-      </div>
+      )}
     </div>
   );
 }
