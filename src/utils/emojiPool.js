cc7bc58e99c9b3097588d839161b7a2ed3996bd8
alexakode <alexanderm.kodehode@gmail.com>
2025-10-03 14:24:54 +0200

    Start expansion with 40 emoji

---
+++ src/utils/emojiPool.js
@@ -0,0 +1,8 @@
+// Eksplisitt liste over emjojier å velge fra for å holde det lokalt og robust. Jeg har brukt Copilot til å foreslå emoji.
+// utils/emojiPool.js
+export const emojiPool = [
+  "🍎", "🍌", "🍇", "🍉", "🍓", "🍒", "🍑", "🥭", "🍍", "🥝",
+  "🥥", "🥦", "🥕", "🌽", "🍅", "🍆", "🥬", "🧄", "🧅", "🥔",
+  "🍔", "🍕", "🌮", "🌯", "🥪", "🍗", "🍖", "🥩", "🍤", "🍣",
+  "🍩", "🍪", "🍰", "🎂", "🧁", "🍫", "🍬", "🍭", "🍮", "🍯"
+];
