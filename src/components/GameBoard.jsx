40fb9fa2a33d4e2380e0801e22179469dc8dfcc9
alexakode <alexanderm.kodehode@gmail.com>
2025-10-04 00:42:59 +0200

    currently works. Todo: limit rows

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,9 @@
+import React from "react";
+import calculateInitialGridSize from "../utils/calculateInitialGridSize";
+import { emojiPool } from "../utils/emojiPool";
+import Card from "./Card";
+export default function GameBoard({ deck }){
+    const { cols } =calculateInitialGridSize();
 const boardStyle = {
   display: 'grid',
   gridTemplateColumns: `repeat(${cols}, minmax(10rem, 1fr))`,
@@ -5,3 +11,9 @@ const boardStyle = {
   maxWidth: `${cols * 11}rem`, // 10rem kort + 1rem gap
   margin: '0 auto',
 };
+return (
+    <div className="game-board" style={boardStyle}>
+        {deck.map((emoji, index) => (<Card key={index} emoji={emoji}/>))}
+    </div>
+)
+}
